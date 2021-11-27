39c33115d8efaa7f919dd022c1f50ce0f083c88c
Adewuyi Mayowa <desbizz@gmail.com>
2021-11-26 21:08:40 -0600

    test for update_Quality

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,7 +13,8 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
-function update_quality() {
+
+function update_quality(items) {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
@@ -59,8 +60,9 @@ function update_quality() {
       }
     }
   }
+  return  items
 }
-
+//console.log(update_quality())
 module.exports = {
   update_quality,
   Item,
--- test/item.test.js
+++ test/item.test.js
@@ -1,20 +1,40 @@
-var {update_quality,items,Item}= require('../src/gilded_rose')
+const { expect } = require('@jest/globals');
+var {update_quality,Item}= require('../src/gilded_rose')
 jest.mock('../src/gilded_rose', () => {
     const originalModule = jest.requireActual('../src/gilded_rose');
-
     return {
       __esModule: true,
       ...originalModule,  
     };
   });
-
+ var items = []
   
  test("Test Item function to add new item", function() {
-    items.push(new Item('New Item', 10, 10));
-    let new_item = items[items.length-1]
-    expect(items[items.length-1]).toBe(new_item)
+    items.push(new Item('+5 Dexterity Vest', 10, 20));
+    items.push(new Item('Aged Brie', 2, 0));
+    items.push(new Item('Elixir of the Mongoose', 5, 7));
+    items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 8, 30));
+    items.push(new Item('Conjured Mana Cake', 3, 6));
+    items.push(new Item('Conjured Mana Cake', 6, 9));
+    items.push(new Item ('New Item',3,10,))
+    expect(items.length).toBe(9)
 
   });
 
+  test('Test result after updating all items Quality', async() => {
+    const days =10
+    const result ={}
+
+    for  (let i = 0; i < days; i++){
+       result[i]=update_quality(items) 
+       console.log('Array'+[i],result[i])
+       expect(result[i]).toMatchSnapshot();     
+    }
+   
+    
+
+  });
   
   
