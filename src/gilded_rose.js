4962c6a3276ce5c411b9d951b2a3ddd4d783af90
Adewuyi Mayowa <desbizz@gmail.com>
2021-11-26 23:07:51 -0600

    checked if item is Aged Brie

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -16,9 +16,12 @@ items.push(new Item('Conjured Mana Cake', 3, 6));
 let is_Aged_Brie =(item)=>{
   return item.name === 'Aged Brie'
 }
+
+
 function update_quality(items) {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    console.log('Testing thisss',is_Aged_Brie(items[i]))
+    if (!is_Aged_Brie(items[i]) && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
